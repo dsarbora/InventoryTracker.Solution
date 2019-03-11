5f51789331c746cc2b0e10f5d53e2e61c7ff2707
Dave Sarbora <dsarbora@gmail.com>
2019-03-11 14:05:40 -0700

    add empty test files

---
+++ InventoryTracker.Tests/ModelTests/Shipment.Test.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Sytem.Collections.Generic;
+using InventoryTracker.Models;
+
+namespace InventoryTracker.Tests
+{
+    public class ShipmentTest : IDisposable
+    {
+        public ShipmentTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root; password=root;port=8889;database=restaurant_test;";
+        }
+        public void Dispose()
+        {
+            Ingredient.ClearAll();
+            Dish.ClearAll();
+            Shipment.ClearAll();
+            Order.ClearAll();
+        }
+    }
+}
