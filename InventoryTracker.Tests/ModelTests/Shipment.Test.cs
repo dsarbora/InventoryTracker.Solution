87f0e6542508590f689d58da100ba70ee90e0529
Dave Sarbora <dsarbora@gmail.com>
2019-03-12 15:02:50 -0700

    add tests and methods for shipment class

--- InventoryTracker.Tests/ModelTests/Shipment.Test.cs
+++ InventoryTracker.Tests/ModelTests/Shipment.Test.cs
@@ -21,16 +21,85 @@ namespace InventoryTracker.Tests
         }
 
         [TestMethod]
-        public void Find()
+        public void Find_GetsById_Shipment()
         {
             DateTime orderDate = Convert.ToDateTime("3/12/2019");
             Shipment shipment = new Shipment(orderDate);
             shipment.Save();
             Shipment newShipment = Shipment.Find(shipment.GetId());
-            Assert.AreEqual(shipment, newShipment);
-
-
-            
+            Assert.AreEqual(shipment, newShipment);            
         }
+
+        [TestMethod]
+        public void ShipmentConstructor_AssignsDate_DateTime()
+        {
+            DateTime orderDate = Convert.ToDateTime("3/12/2019");
+            Shipment shipment = new Shipment(orderDate);
+            shipment.Save();
+            Assert.AreEqual(orderDate, shipment.GetDate());
+        }
+        [TestMethod]
+        public void GetAll_GetsAllShipments_ShipmentList()
+        {
+            DateTime orderDate = Convert.ToDateTime("3/5/2019");
+            DateTime orderDate2 = Convert.ToDateTime("3/12/2019");
+            Shipment shipment = new Shipment(orderDate);
+            shipment.Save();
+            Shipment shipment2 = new Shipment(orderDate);
+            shipment2.Save();
+            List<Shipment> allShipments = new List<Shipment>{shipment, shipment2};
+            List<Shipment> testList = Shipment.GetAll();
+            CollectionAssert.AreEqual(allShipments, testList);
+        }
+
+        [TestMethod]
+        public void AddIngredient_AddsIngredientToShipment_IngredientList()
+        {
+            DateTime orderDate = Convert.ToDateTime("3/12/2019");
+            Shipment shipment = new Shipment(orderDate);
+            shipment.Save();
+            Ingredient newIngredient = new Ingredient("celery");
+            newIngredient.Save();
+            Ingredient newIngredient2 = new Ingredient("carrots");
+            newIngredient2.Save();
+            shipment.AddIngredient(newIngredient.GetId());
+            shipment.AddIngredient(newIngredient2.GetId());
+            List<Ingredient> allIngredients = new List<Ingredient>{newIngredient, newIngredient2};
+            List<Ingredient> testList = shipment.GetAllIngredients();
+            CollectionAssert.AreEqual (allIngredients, testList);
+        }
+
+        [TestMethod]
+        public void GetShipmentIngredients_GetsIngredients_IngredientList()
+        {
+            DateTime orderDate = Convert.ToDateTime("3/12/2019");
+            Shipment shipment = new Shipment(orderDate);
+            shipment.Save();
+            Ingredient newIngredient = new Ingredient("celery");
+            newIngredient.Save();
+            Ingredient newIngredient2 = new Ingredient("carrots");
+            newIngredient2.Save();
+            shipment.AddIngredient(newIngredient.GetId());
+            shipment.AddIngredient(newIngredient2.GetId());
+            List<Ingredient> allIngredients = new List<Ingredient>{newIngredient, newIngredient2};
+            List<Ingredient> testList = shipment.GetAllIngredients();
+            CollectionAssert.AreEqual (allIngredients, testList);
+        }
+
+        [TestMethod]
+        public void DeleteIngredients_DeletesIngredientFromShipment_EmptyList()
+        {
+            DateTime orderDate = Convert.ToDateTime("3/12/2019");
+            Shipment shipment = new Shipment(orderDate);
+            shipment.Save();
+            Ingredient newIngredient = new Ingredient("celery");
+            newIngredient.Save();
+            shipment.AddIngredient(newIngredient.GetId());
+            shipment.DeleteIngredient(newIngredient.GetId());
+            List<Ingredient> allIngredients = new List<Ingredient>{};
+            List<Ingredient> testList = shipment.GetAllIngredients();
+            CollectionAssert.AreEqual(testList, allIngredients);
+        }
+
     }
 }
