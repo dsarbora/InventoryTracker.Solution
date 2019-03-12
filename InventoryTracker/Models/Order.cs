97936a6f344d1dc0ba73ee07e9811ec94023453e
Dave Sarbora <dsarbora@gmail.com>
2019-03-11 23:48:34 -0700

    add bootstrap and styling

--- InventoryTracker/Models/Order.cs
+++ InventoryTracker/Models/Order.cs
@@ -102,7 +102,7 @@ namespace InventoryTracker.Models
         public override bool Equals(System.Object otherOrder)
         {
             if(!(otherOrder is Order))
-            { return false(); }
+            { return false; }
             
             else
             {
