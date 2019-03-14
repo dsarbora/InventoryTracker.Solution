6bb3e72d047dbdc128bd7b75063f9ed8adea8a69
Guest User <Guest@epicodus-19.local>
2019-03-14 11:12:42 -0700

    edit SQL statements to update ingredients quantity in ingredients table

--- BD.sql
+++ BD.sql
@@ -14,3 +14,49 @@ CREATE TABLE table_orders (id serial PRIMARY KEY, table_number VARCHAR(10), orde
 -- update : add dish_quantity INT --
 -- ALTER TABLE `orders` ADD `dish_quantity` INT NOT NULL AFTER `dish_id`;
 CREATE TABLE orders (id serial PRIMARY KEY, table_order_id int, dish_id int, dish_quantity INT);
+
+
+
+-- DON'T RUN : THAT'S ONLY FOR TESTING ------
+
+--------------- ORDERS -----------
+-- On insert
+INSERT INTO orders...;
+
+UPDATE ing SET ing.quantity = ing.quantity - ord.total
+FROM ingredients ing JOIN 
+(SELECT i_d.ingredient_id AS ingredient_id, i_d.quantity * dish_quantity AS total 
+FROM ingredients_dishes i_d JOIN orders ON i_d.dish_id=orders.dish_id 
+WHERE table_order_id=@tableOrderId and orders.dish_id=@dish_id) ord 
+ON ing.id=ord.ingredient_id
+
+-- On update and delete
+UPDATE ing SET ing.quantity = ing.quantity - ord.total
+FROM ingredients ing JOIN 
+(SELECT i_d.ingredient_id AS ingredient_id, i_d.quantity * (@newQuantity - dish_quantity) AS total 
+FROM ingredients_dishes i_d JOIN orders ON i_d.dish_id=orders.dish_id 
+WHERE table_order_id=@tableOrderId and orders.dish_id=@dish_id) ord 
+ON ing.id=ord.ingredient_id
+
+UPDATE / DELETE orders...;
+
+-------------- SHIPMENTS ------------
+
+-- On insert
+UPDATE ing SET ing.quantity = ing.quantity + i_s.quantity FROM 
+
+
+
+
+
+
+
+UPDATE 
+ingredients ing INNER JOIN 
+(SELECT i_d.ingredient_id AS ingredient_id, i_d.ingredient_quantity * (@newQuantity - dish_quantity) AS total 
+FROM ingredients_dishes i_d JOIN orders ON i_d.dish_id=orders.dish_id 
+WHERE table_order_id=@id and orders.dish_id=@dish_id) ord 
+ON ing.id=ord.ingredient_id
+SET ing.quantity = ing.quantity - ord.total;
+
+UPDATE ingredients ing INNER JOIN (SELECT i_d.ingredient_id AS ingredient_id, i_d.ingredient_quantity * (@newQuantity - dish_quantity) AS total FROM ingredients_dishes i_d JOIN orders ON i_d.dish_id=orders.dish_id WHERE table_order_id=@id and orders.dish_id=@dish_id) ord ON ing.id=ord.ingredient_id SET ing.quantity = ing.quantity - ord.total;
